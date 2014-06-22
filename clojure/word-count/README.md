e5f7e3fcad0ebe6ef737d9845b1e18393a67930a
trikitrok <trikitrok@gmail.com>
2014-06-22 20:54:35 +0200

    Did word-count exercise

---
+++ clojure/word-count/README.md
@@ -0,0 +1,20 @@
+# Word Count
+
+Write a program that given a phrase can count the occurrences of each word in that phrase.
+
+For example for the input `"olly olly in come free"`
+
+```plain
+olly: 2
+in: 1
+come: 1
+free: 1
+```
+
+
+
+## Source
+
+The golang tour [view source](http://tour.golang.org)
+
+http://exercism.io/submissions/cd43ba67618f8c0ecee2d338
---
+++ clojure/word-count/word-count.clj
@@ -0,0 +1,30 @@
+(ns word-count
+  (:require [clojure.string :as str]))
+
+(defn word-count [sentence]
+  (let 
+    [count-word-occurrences 
+     (fn [counted-words word] 
+       (let 
+         [add-new-word 
+          (fn [] (assoc counted-words word 1))
+          
+          add-occurrence-to-word 
+          (fn [] (assoc counted-words word (+ (get counted-words word) 1)))]
+         
+         (if (contains? counted-words word)
+           (add-occurrence-to-word)
+           (add-new-word))))
+     
+     extract-words-from 
+     (fn [sentence] 
+       (let
+         [remove-punctuation (comp (partial apply str)
+                                   (partial filter 
+                                            #(or (Character/isLetter %) 
+                                                 (Character/isSpace %)
+                                                 (Character/isDigit %))))]
+         
+         (str/split (remove-punctuation (str/lower-case sentence)) #"\s+")))]
+    
+    (reduce count-word-occurrences {} (extract-words-from sentence))))
