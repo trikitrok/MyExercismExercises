5eef37bb9c9d144d158c07a5ba777185e9d05bba
trikitrok <trikitrok@gmail.com>
2014-06-25 11:41:48 +0200

    Deleted comment

--- clojure/beer-song/beer.clj
+++ clojure/beer-song/beer.clj
@@ -1,9 +1,6 @@
 (ns beer
   (:require [clojure.string :as str]))
 
-; "8 bottles of beer on the wall, 8 bottles of beer.\n
-; Take one down and pass it around, 7 bottles of beer on the wall.\n")
-
 (defn no-more-bottles? [num-of-bottles]
   (= num-of-bottles 0))
 
