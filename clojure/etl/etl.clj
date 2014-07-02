14ac926928affd0ef281de7f48d3b5e429c73a2f
trikitrok <trikitrok@gmail.com>
2014-07-03 00:50:51 +0200

    Another version of etl

--- clojure/etl/etl.clj
+++ clojure/etl/etl.clj
@@ -1,15 +1,14 @@
-(ns etl)
+(ns etl (:require [clojure.string :as str]))
 
 (defn transform [data-set]
-  (reduce 
-    (fn [data [letters num]] 
-      (reduce
-        (fn [data letter]
-          (assoc 
-            data
-            (clojure.string/lower-case letter) 
-            num))
-        data
-        letters))
-    {}
-    (zipmap (vals data-set) (keys data-set))))
+  (let 
+    [assoc-pairs 
+     (fn [data [letters num]]
+       (reduce
+         #(assoc %1 (str/lower-case %2) num)
+         data
+         letters))]
+    (reduce 
+      assoc-pairs
+      {}
+      (zipmap (vals data-set) (keys data-set)))))
