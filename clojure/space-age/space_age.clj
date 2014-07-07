ca8e8d98538bcb69b222b89b0162a68c460ed64c
trikitrok <trikitrok@gmail.com>
2014-07-08 00:25:32 +0200

    Refactor removed magic number

--- clojure/space-age/space_age.clj
+++ clojure/space-age/space_age.clj
@@ -3,25 +3,39 @@
 (defn on-earth [seconds]
   (let 
     [seconds-per-earth-year 31557600.0]
-  (/ seconds seconds-per-earth-year)))
+    (/ seconds seconds-per-earth-year)))
 
 (defn on-mercury [seconds] 
-  (/ (on-earth seconds) 0.2408467))
+  (let 
+    [orbital-period-in-earth-years 0.2408467]
+    (/ (on-earth seconds) orbital-period-in-earth-years)))
 
 (defn on-venus [seconds] 
-  (/ (on-earth seconds) 0.61519726))
+  (let
+    [orbital-period-in-earth-years 0.61519726]
+    (/ (on-earth seconds) orbital-period-in-earth-years)))
 
 (defn on-mars [seconds] 
-  (/ (on-earth seconds) 1.8808158))
+  (let
+    [orbital-period-in-earth-years 1.8808158]
+    (/ (on-earth seconds) orbital-period-in-earth-years)))
 
 (defn on-jupiter [seconds] 
-  (/ (on-earth seconds) 11.862615))
+  (let
+    [orbital-period-in-earth-years 11.862615]
+    (/ (on-earth seconds) orbital-period-in-earth-years)))
 
 (defn on-saturn [seconds] 
-  (/ (on-earth seconds) 29.447498))
+  (let
+    [orbital-period-in-earth-years 29.447498]
+    (/ (on-earth seconds) orbital-period-in-earth-years)))
 
 (defn on-uranus [seconds] 
-  (/ (on-earth seconds) 84.016846))
+  (let
+    [orbital-period-in-earth-years 84.016846]
+    (/ (on-earth seconds) orbital-period-in-earth-years)))
 
 (defn on-neptune [seconds] 
-  (/ (on-earth seconds) 164.79132))
+  (let
+    [orbital-period-in-earth-years 164.79132]
+    (/ (on-earth seconds) orbital-period-in-earth-years)))
