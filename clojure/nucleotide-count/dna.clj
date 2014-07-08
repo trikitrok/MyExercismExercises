ccd8bd740af2f5c1131753237c71fba7021e3d7c
trikitrok <trikitrok@gmail.com>
2014-07-08 11:53:56 +0200

    Simplified code using frequencies function and made two defs private

--- clojure/nucleotide-count/dna.clj
+++ clojure/nucleotide-count/dna.clj
@@ -1,22 +1,11 @@
 (ns dna)
 
-(def dna-nucleotides #{\A, \T, \C, \G})
+(def ^:private dna-nucleotides #{\A, \T, \C, \G})
 
-(def nucleotides (conj dna-nucleotides \U))
+(def ^:private nucleotides (conj dna-nucleotides \U))
 
 (defn nucleotide-counts [strand]
-  (let
-    [counted-nucleotides 
-     (zipmap dna-nucleotides (repeat (count dna-nucleotides) 0))
-     
-     count 
-     (fn [counted-nucleotides nucleotide]
-       (assoc 
-         counted-nucleotides 
-         nucleotide
-         (+ (get counted-nucleotides nucleotide) 1)))]
-    
-    (reduce count counted-nucleotides strand)))
+  (merge {\A 0, \T 0, \C 0, \G 0} (frequencies strand)))
 
 (defn count [nucleotide strand] 
   (if (contains? nucleotides nucleotide)
