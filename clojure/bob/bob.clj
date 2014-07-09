30a367ebcb864d37c5521b1bb8b3d3d59651aa8a
trikitrok <trikitrok@gmail.com>
2014-07-09 02:24:09 +0200

    Indented code

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -7,17 +7,17 @@
      (fn []
        (let
          [letters (filter #(Character/isLetter %) sentence)]
-       (and (not (empty? letters))
-            (every? #(Character/isUpperCase %) letters))))
-
+         (and (not (empty? letters))
+              (every? #(Character/isUpperCase %) letters))))
+     
      asking?
      (fn []
        (= (last sentence) \?))
-
+     
      saying-nothing? #(str/blank? sentence)]
-
-  (cond
-   (shouting?) "Woah, chill out!"
-   (asking?) "Sure."
-   (saying-nothing?) "Fine. Be that way!"
-   :else "Whatever.")))
+    
+    (cond
+      (shouting?) "Woah, chill out!"
+      (asking?) "Sure."
+      (saying-nothing?) "Fine. Be that way!"
+      :else "Whatever.")))
