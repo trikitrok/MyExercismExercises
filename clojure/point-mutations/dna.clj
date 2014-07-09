954dedbef71c07a2fee727de074f747a260870aa
trikitrok <trikitrok@gmail.com>
2014-07-09 02:25:00 +0200

    Reformatted code

--- clojure/point-mutations/dna.clj
+++ clojure/point-mutations/dna.clj
@@ -1,9 +1,10 @@
 (ns dna)
 
 (defn hamming-distance [strand1 strand2]
-  (reduce + 
-          (map 
-            (fn [base1 base2]
-              (if (= base1 base2) 0 1))
-            strand1 
-            strand2)))
+  (reduce 
+    + 
+    (map 
+      (fn [base1 base2]
+        (if (= base1 base2) 0 1))
+      strand1 
+      strand2)))
