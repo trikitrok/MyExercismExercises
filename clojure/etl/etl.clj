a63fcc534353c6b22b5a8ae08bc8ee4b2fba8609
trikitrok <trikitrok@gmail.com>
2014-08-06 19:26:34 +0200

    Used a private function to simplify transform a bit

--- clojure/etl/etl.clj
+++ clojure/etl/etl.clj
@@ -1,23 +1,24 @@
-(ns etl (:require [clojure.string :as str]))
+(ns etl 
+  (:require [clojure.string :as str]))
+
+(defn- associate-score-to-letters 
+  [scores-per-letter [letters score]]
+  (let
+    [associate-score-to-letter
+     (fn [scores-per-letter letter]
+       (assoc 
+         scores-per-letter 
+         (str/lower-case letter)
+         score))]
+    (reduce
+      associate-score-to-letter
+      scores-per-letter
+      letters)))
 
 (defn transform [letters-per-score]
-  (let 
-    [associate-score-to-letters
-     (fn [scores-per-letter [letters score]]
-       (let
-         [associate-score-to-letter
-          (fn [scores-per-letter letter]
-            (assoc 
-              scores-per-letter 
-              (str/lower-case letter)
-              score))]
-         (reduce
-           associate-score-to-letter
-           scores-per-letter
-           letters)))]
-    (reduce 
-      associate-score-to-letters
-      {}
-      (zipmap 
-        (vals letters-per-score) 
-        (keys letters-per-score)))))
+  (reduce 
+    associate-score-to-letters
+    {}
+    (zipmap 
+      (vals letters-per-score) 
+      (keys letters-per-score))))
