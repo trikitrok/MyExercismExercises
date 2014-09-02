23f1c9907b98ccf03c06cb055f3b6aca3215dd61
trikitrok <trikitrok@gmail.com>
2014-09-02 22:50:21 +0200

    Made remove-punctuation a separated function

--- clojure/word-count/word-count.clj
+++ clojure/word-count/word-count.clj
@@ -1,15 +1,15 @@
 (ns word-count
   (:require [clojure.string :as str]))
 
+(def remove-punctuation 
+  (comp (partial apply str)
+        (partial filter 
+                 #(or (Character/isLetter %) 
+                      (Character/isSpace %)
+                      (Character/isDigit %)))))
+
 (defn- extract-words-from [sentence] 
-  (let
-    [remove-punctuation 
-     (comp (partial apply str)
-           (partial filter 
-                    #(or (Character/isLetter %) 
-                         (Character/isSpace %)
-                         (Character/isDigit %))))]
-    (str/split (remove-punctuation (str/lower-case sentence)) #"\s+")))
+  (str/split (remove-punctuation (str/lower-case sentence)) #"\s+"))
 
 (defn word-count [sentence]
   (frequencies (extract-words-from sentence)))
