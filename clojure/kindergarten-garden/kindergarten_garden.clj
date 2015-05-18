6f8f284569f5903bc38f6ae717bd1d7ab9935f51
trikitrok <trikitrok@gmail.com>
2015-05-18 23:57:05 +0200

    Kindergarten garden problem

---
+++ clojure/kindergarten-garden/kindergarten_garden.clj
@@ -0,0 +1,48 @@
+(ns kindergarten-garden)
+
+(def ^:private plants
+  {\R :radishes \C :clover \G :grass \V :violets})
+
+(def ^:private default-children
+  [:alice :bob :charlie :david
+   :eve :fred :ginny :harriet
+   :ileana :joseph :kincaid :larry])
+
+(def ^:private get-plants
+  (partial map #(get plants %)))
+
+(defn- plants-rows [diagram]
+  (map #(vec (get-plants %))
+       (clojure.string/split diagram #"\n")))
+
+(defn- plants-per-child-in-row [row-index rows]
+  (vec
+    (flatten
+      (map #(vector (get % row-index) (get % (inc row-index))) rows))))
+
+(defn- plants-per-child [diagram]
+  (let [num-cols (.indexOf diagram "\n")]
+    (map #(plants-per-child-in-row (* 2 %) (plants-rows diagram))
+         (range 0 (quot num-cols 2)))))
+
+(def ^:private lowercase-all
+  (partial map #(clojure.string/lower-case %)))
+
+(def ^:private strs->keys
+  (partial map #(keyword %)))
+
+(defn children-keys [children-strs]
+  (->> children-strs
+       lowercase-all
+       sort
+       strs->keys))
+
+(defn- make-garden [children diagram]
+  (zipmap children (plants-per-child diagram)))
+
+(defn garden
+  ([diagram]
+   (make-garden default-children diagram))
+
+  ([diagram children-strs]
+   (make-garden (children-keys children-strs) diagram)))
