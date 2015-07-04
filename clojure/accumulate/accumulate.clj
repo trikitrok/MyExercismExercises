9987dcb0e41a3e14ec74acd04d6ca1593cc2c14b
trikitrok <trikitrok@gmail.com>
2015-07-04 20:48:17 +0200

    Added a recursive solution (commented)

--- clojure/accumulate/accumulate.clj
+++ clojure/accumulate/accumulate.clj
@@ -1,4 +1,13 @@
- (ns accumulate)
+(ns accumulate)
 
- (defn accumulate [func coll]
-   (reduce #(conj %1 (func %2)) [] coll))
+(defn accumulate [func coll]
+  (reduce #(conj %1 (func %2)) [] coll))
+
+;(defn accumulate [func coll]
+;  (let [accumulate-rec
+;        (fn [acc [first-elem & rest-elems]]
+;          (if (nil? first-elem)
+;            acc
+;            (recur (conj acc (func first-elem))
+;                   rest-elems)))]
+;    (accumulate-rec [] coll)))
