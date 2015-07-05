06481924b85ae4b4d9ec376c2253fa3183d42c8d
trikitrok <trikitrok@gmail.com>
2015-07-05 14:05:39 +0200

    Another version

--- clojure/binary-search-tree/binary_search_tree.clj
+++ clojure/binary-search-tree/binary_search_tree.clj
@@ -11,8 +11,8 @@
   (let [add-node
         (fn [location node]
           (assoc tree location
-            (if node (insert val node)
-                     (singleton val))))]
+                      (if node (insert val node)
+                               (singleton val))))]
     (if (<= val (value tree))
       (add-node :left (left tree))
       (add-node :right (right tree)))))
@@ -20,17 +20,24 @@
 (defn from-list [[root & rest-nodes]]
   (reduce #(insert %2 %1) (singleton root) rest-nodes))
 
-(defn to-list
-  ([tree] (to-list [] tree))
-  ([acc tree]
-    (cond
-      (and (left tree) (not (right tree)))
-      (conj (to-list acc (left tree)) (value tree))
+(defn to-list [tree]
+  (if tree
+    (concat (to-list (left tree))
+            [(value tree)]
+            (to-list (right tree)))
+    []))
 
-      (and (not (left tree)) (right tree))
-      (to-list (conj acc (value tree)) (right tree))
-
-      (and (left tree) (right tree))
-      (to-list (conj (to-list acc (left tree)) (value tree)) (right tree))
-
-      :else (conj acc (value tree)))))
+;(defn to-list
+;  ([tree] (to-list [] tree))
+;  ([acc tree]
+;    (cond
+;      (and (left tree) (not (right tree)))
+;      (conj (to-list acc (left tree)) (value tree))
+;
+;      (and (not (left tree)) (right tree))
+;      (to-list (conj acc (value tree)) (right tree))
+;
+;      (and (left tree) (right tree))
+;      (to-list (conj (to-list acc (left tree)) (value tree)) (right tree))
+;
+;      :else (conj acc (value tree)))))
